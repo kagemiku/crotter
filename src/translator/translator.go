712570947a718074afa48fb192b05d91ae804576
kage <kagemiku@gmail.com>
2018-04-17 21:06:28 +0900

    Format tweet text properly even if it has some newlines

--- src/translator/translator.go
+++ src/translator/translator.go
@@ -4,7 +4,19 @@ import (
 	"errors"
 	"fmt"
 	"github.com/dghubble/go-twitter/twitter"
+	"strings"
 )
+
+const margin = "  "
+
+func giveMargin(text string) string {
+	marginNL := fmt.Sprintf("\n%s", margin)
+	return strings.Replace(text, "\n", marginNL, -1)
+}
+
+func decolateTweetText(text string) string {
+	return giveMargin(text)
+}
 
 func TranslateTweet(tweet *twitter.Tweet) (string, error) {
 	if tweet == nil {
@@ -13,9 +25,9 @@ func TranslateTweet(tweet *twitter.Tweet) (string, error) {
 
 	tweetString := ""
 
-	tweetString += fmt.Sprintf("  %s @%s %s\n", tweet.User.Name, tweet.User.ScreenName, tweet.CreatedAt)
-	tweetString += fmt.Sprintf("  %s\n\n", tweet.Text)
-	tweetString += fmt.Sprintf("  RT: %d  Like: %d\n\n", tweet.RetweetCount, tweet.FavoriteCount)
+	tweetString += fmt.Sprintf("%s%s @%s %s\n", margin, tweet.User.Name, tweet.User.ScreenName, tweet.CreatedAt)
+	tweetString += fmt.Sprintf("%s%s\n\n", margin, decolateTweetText(tweet.Text))
+	tweetString += fmt.Sprintf("%sRT: %d  Like: %d\n\n", margin, tweet.RetweetCount, tweet.FavoriteCount)
 
 	return tweetString, nil
 }
